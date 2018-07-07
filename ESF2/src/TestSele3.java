d06fb0eb215dfc481719334618105f8346244ab6
yperaboina <40818100+yperaboina@users.noreply.github.com>
2018-07-07 10:44:10 +0530

    adding testSele3 calss to staging

---
+++ ESF2/src/TestSele3.java
@@ -0,0 +1,10 @@
+
+public class TestSele3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.println(" created test TestSele3 in staging");
+	
+	}
+
+}
