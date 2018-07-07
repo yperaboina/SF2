a4be17a0ed94ea245a8bf163520c8517c46f4968
yperaboina <40818100+yperaboina@users.noreply.github.com>
2018-07-07 10:32:15 +0530

    added 2nd program

---
+++ ESF2/src/TestSele2.java
@@ -0,0 +1,9 @@
+
+public class TestSele2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.println(" created test TestSele2  and the same in main method in master");
+	}
+
+}
