f2e83cd6f82a9d3c3f7e14865a610f812a45b3fd
yperaboina <40818100+yperaboina@users.noreply.github.com>
2018-07-13 12:31:33 +0530

    Removed 3rd comment

--- ESF2/src/TestSele1.java
+++ ESF2/src/TestSele1.java
@@ -5,7 +5,7 @@ public class TestSele1 {
 		// TODO Auto-generated method stub
   System.out.println("main method started of master");
 		 System.out.println("updated file to poll automatically");
-		 System.out.println("updated file to poll automatically2");
+		
 		 
 	}
 
