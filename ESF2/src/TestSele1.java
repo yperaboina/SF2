f814675eec73e3d32681c1884000c26b9350ee32
yperaboina <40818100+yperaboina@users.noreply.github.com>
2018-07-13 15:50:41 +0530

    removed all comments lines

--- ESF2/src/TestSele1.java
+++ ESF2/src/TestSele1.java
@@ -6,12 +6,8 @@ public class TestSele1 {
 		
 		// TODO Auto-generated method stub
   		System.out.println("main method started of master");
-		 System.out.println("updated file to poll automatically");
-		System.out.println("changed job name to GitCommitsTest");
-		System.out.println("added another comment line by vivek");
+		 System.out.println("removed all files to execute");
 		
-		System.out.println("Hello Venkat");
-		System.out.println("Hello Karthik");
 		 
 	}
 
