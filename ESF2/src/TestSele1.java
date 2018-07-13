fea2fa9358b6c01af82ff1894701bdb0a3bb057f
yperaboina <40818100+yperaboina@users.noreply.github.com>
2018-07-13 14:13:15 +0530

    added hello world line

--- ESF2/src/TestSele1.java
+++ ESF2/src/TestSele1.java
@@ -2,12 +2,14 @@
 public class TestSele1 {
 
 	public static void main(String[] args) {
+		
+		
 		// TODO Auto-generated method stub
-  System.out.println("main method started of master");
+  		System.out.println("main method started of master");
 		 System.out.println("updated file to poll automatically");
 		System.out.println("changed job name to GitCommitsTest");
 		System.out.println("added another comment line by vivek");
-		
+		System.out.println("Hello world");
 		 
 	}
 
