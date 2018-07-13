7297669769d4ec4e86127635a563ea3570d92dcd
yperaboina <40818100+yperaboina@users.noreply.github.com>
2018-07-13 15:56:45 +0530

    will go

--- ESF2/src/TestSele1.java
+++ ESF2/src/TestSele1.java
@@ -7,6 +7,7 @@ public class TestSele1 {
 		// TODO Auto-generated method stub
   		System.out.println("main method started of master");
 		 System.out.println("removed all files to execute");
+		System.out.println("I will go now");
 		
 		 
 	}
