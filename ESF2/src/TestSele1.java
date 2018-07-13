4c51fffeeb9817c21dd8b73567e1c0046bd33323
yperaboina <40818100+yperaboina@users.noreply.github.com>
2018-07-13 12:29:00 +0530

    to update automatically

--- ESF2/src/TestSele1.java
+++ ESF2/src/TestSele1.java
@@ -4,6 +4,8 @@ public class TestSele1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
   System.out.println("main method started of master");
+		 System.out.println("updated file to poll automatically");
+		 System.out.println("updated file to poll automatically2");
 		 
 	}
 
