b0536a5acce841ad24ee4d227564f95386809929
yperaboina <40818100+yperaboina@users.noreply.github.com>
2018-07-13 12:38:57 +0530

    changed job name in jenkins

--- ESF2/src/TestSele1.java
+++ ESF2/src/TestSele1.java
@@ -5,6 +5,7 @@ public class TestSele1 {
 		// TODO Auto-generated method stub
   System.out.println("main method started of master");
 		 System.out.println("updated file to poll automatically");
+		System.out.println("changed job name to GitCommitsTest");
 		
 		 
 	}
